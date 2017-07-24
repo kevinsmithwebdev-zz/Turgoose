0117b3ab9cb06ee51660708a70c5173877e2b159
Kevin Smith <ksjazzguitar@yahoo.com>
2017-07-24 12:45:56 -0700

    adding calendar update

--- js/calendar.js
+++ js/calendar.js
@@ -1,18 +1,117 @@
 $(document).ready(function () {
 
-  var iframeStr = '<iframe ' +
-        'src="https://calendar.google.com/calendar/embed' +
-        '?src=rachelturgoosemusic%40gmail.com' +
-        '&mode=AGENDA' +
-        '&bgcolor=%23ffffff' +
-        '&ctz=America/Chicago" ' +
-        'style="border: 0" ' +
-        'width="800" ' +
-        'height="400" ' +
-        'frameborder="0" ' +
-        'scrolling="yes"' +
-        'ALLOWTRANSPARENCY="true"' +
-        '></iframe>';
+  const CAL_MSG = "Come hear Rachel Play ...";
 
-  $('#calendar-area').append(iframeStr);
+  var $calArea = $('#calendar-area');
+
+  const DATE_COL = 'col-xs-2';
+  const TIME_COL = 'col-xs-2';
+  const LOC_COL = 'col-xs-4';
+  const SUM_COL = 'col-xs-4';
+
+  var accountStr = 'rachelturgoosemusic@gmail.com';
+  var apiKey = 'AIzaSyB70OG0v_0v5jFyYue7m1cY7VZAlKoEnTQ';
+
+  var formattedDate = ISODateString(new Date());
+  var numberEvents = 10;
+
+
+  var urlStr =  'https://www.googleapis.com/calendar/v3/calendars/' +
+                accountStr + '/events' +
+                '?singleEvents=true' +
+                '&timeMin=' + formattedDate +
+                '&maxResults=' + numberEvents +
+                '&orderBy=startTime' +
+                '&sortorder=ascending' +
+                // 'items(description,end(date,dateTime),location,start(date,dateTime,time))' +
+                '&key=' + apiKey;
+
+
+  // add cal title
+
+  $calArea.append('<div class="row cal-title">' + CAL_MSG + '</div>');
+
+
+  // add cal header
+
+  $calArea.append('<div class="row cal-header">' +
+                    '<div class=' + DATE_COL + '>' + "Date" + "</div>" +
+                    '<div class=' + TIME_COL + '>' + "Time" + "</div>" +
+                    '<div class=' + LOC_COL + '>' + "Location" + "</div>" +
+                    '<div class=' + SUM_COL + '>' + "Description" + "</div>" +
+                  '</div>\n');
+
+
+  $.getJSON(urlStr, function(data) {
+    var events = data.items;
+
+    for (var i=0; i<events.length; i++) {
+      var event = parseEvent(events[i]);
+      $calArea.append(calendarLine("cal-item", event.date, event.time, event.location, event.summary));
+    }
+    $calArea.append('<div class="cal-full-link"><a target="_blank" href="https://calendar.google.com/calendar/embed?src=rachelturgoosemusic%40gmail.com&ctz=America/Chicago">See Rachel\'s full calendar.</a></div>');
+
+  });
+
+  //***************************************
+  //***************************************
+  //***************************************
+
+  function calendarLine(rowClass, date, time, loc, sum) {
+
+    return '<div class="row ' + rowClass + '">\n' +
+              '<div class="' + DATE_COL + '">' + date + '</div>\n' +
+              '<div class="' + TIME_COL + '">' + time + '</div>\n' +
+              '<div class="' + LOC_COL + '">' +
+                '<a target="_blank" href="http://maps.google.com/maps?q=' +
+                encodeURIComponent(loc) + '">' + loc + '</a></div>\n' +
+              '<div class="' + SUM_COL + '">' + sum + '</div>\n' +
+            '</div>\n';
+  }
+
+
+  function parseEvent(eventObj) {
+    var event = {};
+
+    const DOW = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const MOY = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    var hasTime = eventObj.start.hasOwnProperty('dateTime');
+    var rawDateStr=(hasTime)?eventObj.start.dateTime:eventObj.start.date;
+
+    var year = +rawDateStr.substring(0,4);
+    var month = +rawDateStr.substring(5,7) - 1;
+    var date = +rawDateStr.substring(8,10);
+
+    var day = (new Date(year, month, date)).getDay();
+
+    event.date = DOW[day] + ", " + MOY[month] + " " + date;
+
+    if (hasTime)
+      event.time = timeStr(+eventObj.start.dateTime.substring(11,13), +eventObj.start.dateTime.substring(14,16)) + "-"
+                      + timeStr(+eventObj.end.dateTime.substring(11,13), +eventObj.end.dateTime.substring(14,16));
+     else
+      event.time = "";
+
+    event.location = eventObj.location;
+    event.summary = eventObj.summary;
+
+    return event;
+  }
+
+  const timeStr = (hours, mins) => (hours%12||12) + (mins?(":"+mins):"") + (hours<12?"am":"pm");
+
+
+  function ISODateString(d) {
+    const pad = (n) => { return n<10 ? '0'+n : n }
+
+    return d.getUTCFullYear()+'-'
+      + pad(d.getUTCMonth()+1)+'-'
+      + pad(d.getUTCDate())+'T'
+      + pad(d.getUTCHours())+':'
+      + pad(d.getUTCMinutes())+':'
+      + pad(d.getUTCSeconds())+'Z';
+  }
+
+
 });
--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
 $(document).ready(function () {
+
+  const BG_PIC_NUM = 3; // number of BG pics, named img/BGxx.jpg
+  const BG_INTERVAL = 10 * 1000; // delay of BG pic switch
+  const BG_FADE = 1000; // delay of BG pic fade
+
   var menuList = [
-      { name: 'Home', file: 'index.html' },
-      { name: 'Media', file: 'media.html' },
-      { name: 'Gallery', file: 'gallery.html' },
-      { name: 'Bio', file: 'bio.html' },
-      // { name: 'Press Kit', file: 'presskit.html' },
-      { name: 'Teaching', file: 'teaching.html' },
-      { name: 'Partners', file: 'partners.html' },
-      { name: 'Contact', file: 'contact.html' }
-    ];
+    { name: 'Home', file: '' },
+    { name: 'Media', file: 'media.html' },
+    { name: 'Gallery', file: 'gallery.html' },
+    { name: 'Bio', file: 'bio.html' },
+    { name: 'Teaching', file: 'teaching.html' },
+    { name: 'Partners', file: 'partners.html' },
+    { name: 'Contact', file: 'contact.html' }
+  ];
 
   var footerList = ['Rachel Turgoose &copy;2017',
-        '<a href="mailto:rachelturgoosemusic@gmail.com?Subject=inquiry%20from%20web%20page" target="_top">rachelturgoosemusic@gmail.com</a> ',
-        '484-459-8884'];
+      '<a href="mailto:rachelturgoosemusic@gmail.com?Subject=inquiry%20from%20web%20page" target="_top">rachelturgoosemusic@gmail.com</a> ',
+      '484-459-8884'];
 
-  pageFName = window.location.pathname.split('/').pop();
+  var pageFName = location.pathname.split('/').pop();
 
   var titleName = "Rachel Turgoose";
   var titleDescr = "Vocals. Woodwinds. Educator.";
+
 
   initNav();
 
@@ -25,17 +30,18 @@ $(document).ready(function () {
 
   addPageIdent();
 
-  //************************************
+//************************************
+//************************************
+//************************************
 
   function initNav() {
-    // insert the nav buttons on current page
-
+  // insert the nav buttons on current page
     var menuStr = '<div class="container-fluid"><div class="navbar-header">' +
           '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">' +
           '<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>' +
           '</button><a class="navbar-brand" href="#">rachelturgoose.com</a></div>';
-
     menuStr += '<div class="collapse navbar-collapse" id="myNavbar"><ul class="nav navbar-nav">';
+
     for (var i = 0 ; i < menuList.length ; i++) {
       menuStr += '<li><a href="' + menuList[i].file + '">' + menuList[i].name + '</a></li>';
     }
@@ -52,40 +58,59 @@ $(document).ready(function () {
     });
   } // initNav()
 
+//************************************
+
   function initFooter() {
-      // insert the footer on current page
+    // insert the footer on current page
+    var footerStr = '<div class="footer-container"><hr>';
 
-      var footerStr = '<div class="footer-container"><hr>';
-      for (var i = 0 ; i < footerList.length ; i++) {
-        footerStr += '<span>' + footerList[i] + '</span>';
-      }
-
-      footerStr += '</div>';
-      $('footer').html(footerStr);
+    for (var i = 0 ; i < footerList.length ; i++) {
+      footerStr += '<span>' + footerList[i] + '</span>';
     }
+    footerStr += '</div>';
+
+    $('footer').html(footerStr);
+
+  } // initFooter
+
+//************************************
 
   function addPageIdent() {
+
     var pageIdent = menuList.find( function(item) {
       return item.file === pageFName;
-      });
+    });
 
     $('.title-area').append("<span class = 'title'>" + titleName + "</span>");
     $('.title-area').append("<span class = 'sub-title'>" + titleDescr + "</span>");
 
-    if (pageIdent.name !== 'Home') {
+    if (pageIdent && pageIdent.file) {
       $('title').append(" - " + pageIdent.name);
       $('.title').append(" - " + pageIdent.name);
     }
+  } // addPageIdent
 
+//************************************
+
+  // precache images
+  for (var i=1; i<=BG_PIC_NUM; i++) {
+    new Image().src='img/BG' + twoDigit(i) + '.jpg';
   }
 
-  var bgNum = 1;
+  // load first BG image (to replace low-res dummy)
+
+  $('body').css('background-image', 'url(img/BG01.jpg)');
+
+  // set BG change
+  var bgNum = 2;
 
   setInterval( function(){
-      (bgNum==4)?(bgNum=1):(bgNum++);
-      $('body').css('background-image', 'url(' + 'img/BG' + bgNum + '.jpg' + ')');
-    }, 5000);
+    $('body').css('background-image', 'url(' + 'img/BG' + twoDigit(bgNum) + '.jpg' + ')');
+    (bgNum==BG_PIC_NUM)?(bgNum=1):(bgNum++);
+  }, BG_INTERVAL);
 
+}); // document ready
 
-
-});
+function twoDigit(num) {
+  return (num>=10)?num:("0"+ num);
+}
